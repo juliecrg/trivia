0f3f94f1ff29c00a3082605869d2aa2e76069b57
juliecrg <51386270+juliecrg@users.noreply.github.com>
2019-06-05 13:48:20 -0500

    Un HTML junto

---
+++ Trivia 3/app.js
@@ -0,0 +1,56 @@
+// content.innerHTML = "Hola " + inputText.value;
+const userName = document.getElementById("user-name");
+const button1Send = document.getElementById("botonEnviar");
+button1Send.addEventListener("click", nombre);
+
+//llama la funcion nombre
+function nombre(e){
+  //content.innerHTML = "Hola " + inputText.value;
+  document.getElementById('print-name').innerHTML += 'Bienvenidx ' + userName.value;
+  userName.value = "";
+}
+
+
+const buttonSend = document.getElementById("botonEvaluar");
+buttonSend.addEventListener("click", correct)
+const inputsAnswer= Array.from(document.getElementsByClassName("datos"));
+
+function correct(e){
+  let score=0;
+  for(let i=0; i < inputsAnswer.length; i++){
+    if(inputsAnswer[i].checked){
+      let respCorrect=inputsAnswer[i].value;
+        if(respCorrect === "Roy Orbison"){
+          alert("respuesta1 correcta muy bien")
+          score ++;}
+        else if(respCorrect === "Revolver"){
+          alert("respuesta2 correcta muy bien")
+          score ++;}
+        else if(respCorrect === "Pyotr"){
+          alert("respuesta3 correcta muy bien")
+          score ++;}
+        else if(respCorrect === "Titanic"){
+          alert("respuesta1 correcta muy bien")
+          score ++;}
+        else if(respCorrect === "Lo que el viente se llevo"){
+          alert("respuesta2 correcta muy bien")
+          score ++;}
+        else if(respCorrect === "Sean Connery"){
+          alert("respuesta3 correcta muy bien")
+          score ++;}
+        else{
+      }
+    }
+
+  }
+  alert("obtuviste "+ score + " puntos")
+}
+function changeSection(id){ //id recibe el id de la seccion a cambiar:
+ console.log(id)
+// crea hoja de estilo para la seccion correspondiente
+    respCorrect.createElement('style') //crea el elemneto estilo en una variable
+    respCorrect.innerHTML = "section {background-color:yellow;}";
+  // respcorrect.innerHTML =
+    document.body.appendChild(sheet);
+//    }
+}
